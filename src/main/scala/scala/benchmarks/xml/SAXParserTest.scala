e0a122c03b66ee4b84d367515e1ceb7082f5766e
ekalashnikov <eugene.kalashnikov@agoda.com>
2018-06-26 14:37:42 +0700

    add xml tests

--- src/main/scala/scala/benchmarks/xml/SAXParserTest.scala
+++ src/main/scala/scala/benchmarks/xml/SAXParserTest.scala
@@ -19,26 +19,26 @@ object XMLParsersState {
 
   @State(Scope.Thread)
   class XmlData {
-    final val xml = """<ExternalData xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-          <source>spapi</source>
-          <HotelCode>12481</HotelCode>
-          <RoomTypeCode>5ded6fe</RoomTypeCode>
-          <BrandCode>40090TRE</BrandCode>
-          <Adults>2</Adults>
-          <Children>1</Children>
-          <Rooms>1</Rooms>
-          <CheckIn>2017-12-23'T'12:30:45</CheckIn>
-          <CheckOut>2017-12-25'T'12:30:45</CheckOut>
-          <AmountBeforeTax>34.56</AmountBeforeTax>
-          <Tax>24.56</Tax>
-          <ServiceCharge>65.56</ServiceCharge>
-          <ExtraPersonRate>615.56</ExtraPersonRate>
-          <ChainCode>BTY4658</ChainCode>
-          <CurrencyCode>THB</CurrencyCode>
-          <RatePlanCode>23</RatePlanCode>
-          <RateType>someType</RateType>
-          <supplySource>Pull</supplySource>
-        </ExternalData>""".split('\n').map(_.trim.filter(_ >= ' ')).mkString
+    final val xml = """<Data xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+          <field1>api</field1>
+          <field2>12481</field2>
+          <field3>5ded6fe</field3>
+          <field4>40090TRE</field4>
+          <field5>2</field5>
+          <field6>1</field6>
+          <field7>1</field7>
+          <field8>2017-12-23'T'12:30:45</field8>
+          <field9>2017-12-25'T'12:30:45</field9>
+          <field10>34.56</field10>
+          <field11>24.56</field11>
+          <field12>65.56</field12>
+          <field13>615.56</field13>
+          <field14>BTY4658</field14>
+          <field15>THB</field15>
+          <field16>23</field16>
+          <field17>someType</field17>
+          <field18>Pull</field18>
+        </Data>""".split('\n').map(_.trim.filter(_ >= ' ')).mkString
   }
 }
 
