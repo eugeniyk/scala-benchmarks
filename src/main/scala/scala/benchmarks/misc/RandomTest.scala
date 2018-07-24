e4e846a55e1c23b5ef6a682819a6b1ba0c1d0765
ekalashnikov <eugene.kalashnikov@agoda.com>
2018-07-24 09:46:05 +0700

    Add random test

---
+++ src/main/scala/scala/benchmarks/misc/RandomTest.scala
@@ -0,0 +1,43 @@
+package scala.benchmarks.misc
+
+import java.util.concurrent.TimeUnit
+
+import org.openjdk.jmh.annotations.{Benchmark, OutputTimeUnit, Scope, State}
+
+import scala.benchmarks.misc.RandomTestState.{StateWithJavaRandom, StateWithScalaRandom}
+import scala.util.Random
+
+object RandomTestState {
+  @State(Scope.Thread)
+  class StateWithJavaRandom {
+    val random = new java.util.Random()
+  }
+
+  @State(Scope.Thread)
+  class StateWithScalaRandom {
+    val random = new Random()
+  }
+}
+
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+class RandomTest {
+  @Benchmark
+  def useJavaThreadRandom() = {
+    java.util.concurrent.ThreadLocalRandom.current().nextInt()
+  }
+
+  @Benchmark
+  def useScalaThreadRandom() = {
+    scala.concurrent.forkjoin.ThreadLocalRandom.current().nextInt()
+  }
+
+  @Benchmark
+  def useJavaRandom(state: StateWithJavaRandom) = {
+    state.random.nextInt()
+  }
+
+  @Benchmark
+  def useScalaRandom(state: StateWithScalaRandom) = {
+    state.random.nextInt()
+  }
+}
